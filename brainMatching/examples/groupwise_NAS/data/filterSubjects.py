f7721fb71173d7fae81e09e99bd2fcb7a387a942
Abe Jeyapratap <abejeyapratap@gmail.com>
2022-07-27 14:20:20 -0400

    pre-process subject names

---
+++ brainMatching/examples/groupwise_NAS/data/filterSubjects.py
@@ -0,0 +1,28 @@
+import os
+
+files = []
+with open("all_subjects.txt", "r") as f:
+    files = f.read().splitlines()
+
+# DIR = './connectomes2'
+# print(len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))]))
+
+# check which demographics subjects are not in Schaefer
+""" for f in files:
+    if not os.path.isfile(f"connectomes2/{f}.txt"):
+        print(f) """
+
+# intersection of Schaefer connectomes & tobacco demographics
+filtered = []
+for f in files:
+    if os.path.isfile(f"connectomes2/{f}.txt"):
+        filtered.append(f)
+
+# print(len(files), len(filtered))
+
+with open("subjects_filtered.txt", "w") as f:
+    for val in filtered:
+        f.write(val +"\n")
+
+# print(os.path.isfile(f"connectomes2/{files[1]}.txt"))
+# print(all([os.path.isfile(f"connectomes2/{f}.txt") for f in files]))
