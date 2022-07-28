893aabc733c0ca3c2dcfe9118cae7e40bc02e6bb
Abe Jeyapratap <abejeyapratap@gmail.com>
2022-07-28 16:30:02 -0400

    missing file names

--- brainMatching/examples/groupwise_NAS/data/filterSubjects.py
+++ brainMatching/examples/groupwise_NAS/data/filterSubjects.py
@@ -8,9 +8,11 @@ with open("all_subjects.txt", "r") as f:
 # print(len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))]))
 
 # check which demographics subjects are not in Schaefer
-""" for f in files:
-    if not os.path.isfile(f"connectomes2/{f}.txt"):
-        print(f) """
+with open("missing.txt", "w") as file:
+    for f in files:
+        if not os.path.isfile(f"connectomes2/{f}.txt"):
+            print(f)
+            file.write(f +"\n")
 
 # intersection of Schaefer connectomes & tobacco demographics
 filtered = []
@@ -20,9 +22,10 @@ for f in files:
 
 # print(len(files), len(filtered))
 
-with open("subjects_filtered.txt", "w") as f:
+# store filtered connectome names to file
+""" with open("subjects_filtered.txt", "w") as f:
     for val in filtered:
-        f.write(val +"\n")
+        f.write(val +"\n") """
 
 # print(os.path.isfile(f"connectomes2/{files[1]}.txt"))
 # print(all([os.path.isfile(f"connectomes2/{f}.txt") for f in files]))
