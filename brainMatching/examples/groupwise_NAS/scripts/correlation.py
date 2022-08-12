9216ca4252e33dca49f4f9c6f712916cd8c14c38
Abe Jeyapratap <abejeyapratap@gmail.com>
2022-08-12 13:50:13 -0400

    init correl

---
+++ brainMatching/examples/groupwise_NAS/scripts/correlation.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+import scipy.stats as stt
+from helpers import drawCorrelationPlot, drawBoxPlot
+
+tdcPath = "../data/tdc_desikan.txt"
+asdPath = "../data/asd_desikan.txt"
+
+with open(tdcPath, "r") as f:
+    healthyIDs = f.read().splitlines()
+
+with open(asdPath, "r") as f:
+    patientIDs = f.read().splitlines()
+
+healthySize = len(healthyIDs)
+patientSize = len(patientIDs)
+healthyOrder = list(range(healthySize))
+healthy = healthyOrder
+patientsOrder = list(range(healthySize, healthySize+patientSize))
+patients = patientsOrder
+
+controlGroups = [healthy]
+patientGroups = [patients]
+controlGroupNames = ["TDC"]
+patientGroupNames = ["ASD"]
+
+### Load NAS results
+# resultFile = "../experiment/results/NAS_rt_healthy.res"
+resultFile = "../experiment/results_desikan_all/results_norm_desikan/NAS_rt_healthy.res"
+fileContent =  open(resultFile,"r").read().splitlines()
+numNodes = int(fileContent[1].split('\t')[0])
+numSubjects = int(fileContent[1].split('\t')[1])
+measureType = str(fileContent[3]) 
+scoreName = str(fileContent[5]) 
+scores = np.zeros(numSubjects)
+
+# load matching accuracy scores
+for i in range(numSubjects):
+    scores[i] = float(fileContent[7].split('\t')[i])
+
+colors=['#351C4D', '#AB3E16','#849974','#2096BA','#F7DFD4','#F5AB99'] #nightfall, rust, fresh, shutter blue, macaron, tropical pink
+
+outputFolder = "../experiment/plots/correl/"
+outputPath=outputFolder+"any"+"_"+measureType+"_"+"dist"+".png"
+
+
+data = [scores[patients], scores[healthy]]
+dataLabels=['Patient','Healthy']
+
+minY=min([min(l) for l in data])
+maxY=max([max(l) for l in data])
+offset=(maxY-minY)/5.0
+
+yLim=[minY-offset/2.0,maxY+offset] ## make space specific to figure
+# yLim=[67,102] ## use these lines to make the space constant (such as across different plots)
+scoreName="network similarity (%)"
+title = ""
+drawBoxPlot(data,dataLabels,title,outputPath,xLabel='',yLabel=scoreName,colors=colors,rotation=0,plotScatter=True,yLim=yLim,middleLine='median') #Mann-Whitney U test so plot median line in boxplots
